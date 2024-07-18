4927756df3658f72d76a2402cb728817ffd8a27f
parsachow <parsakishwa06@gmail.com>
2024-07-18 03:33:19 -0400

    crud update

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
+from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     #add in fieldtype after serializers
     #everthing will be mapped using serializer according to our models 
     id = serializers.IntegerField(read_only=True) #read_only=True is used to access the id only
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(style={'type': 'textarea'})
     active = serializers.BooleanField()
+    
+    #for POST request - to create data
+    def create(self, validated_data):
+        # Create and return a new `Movie` instance, given the validated data.
+      return Movie.objects.create(**validated_data)
+  
+    def update(self, instance, validated_data): 
+        #when using UPDATE, instance carries the old value and validated data carries the new data sent by the user
+        
+        instance.name = validated_data.get('name', instance.name)
+        #need to pass old instance as the 2nd argument for the validated data as its now being updated with new information
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        
+        instance.save()
+        return instance
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,24 +1,53 @@
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
 # # Create your views here.
 
-@api_view() #By default, it only provides GET req
+@api_view(['GET', 'POST']) #By default, it only provides GET req
 #decorator for working with function based views which provides some functionality like receiving Request or adding context to Response. 
 def movie_list(request):
-    movies = Movie.objects.all() #get all movies
+    if request.method == 'GET':
+        
+        movies = Movie.objects.all() #get all movies
+        #create a variable ex; serialiser to store all the data about the ex; movies and pass movies as complex data into the serializer class 
+        
+        serializer = MovieSerializer(movies, many=True) #When using GET request to fetch all objects/data, use - many=True as the 2nd argument
     
-    #create a variable ex; serialiser to store all the data about the ex; movies and pass movies as complex data into the serializer class 
-    serializer = MovieSerializer(movies, many=True) #When using GET request to fetch all objects/data, use - many=True as the 2nd argument
+        #return a Response. Pass the variable  created to store the data ex; serializer.data as the response
+        return Response(serializer.data)
     
-    #return a Response. Pass the variable  created to store the data ex; serializer.data as the response
-    return Response(serializer.data)
+    elif request.method == 'POST':
+        #since its a POST req, we are getting data from user, we will use serializer, get data from user and SAVE it.
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
     
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
+#PUT/DELETE Req can only be performed on individual items. PATCH req will only update specific fields on an item not the whole thing like PUT. 
+
 def movie_detail(request, pk): #getting a specific object by passing a pk
-    movie = Movie.objects.get(pk=pk) #passing pk=pk as we are getting a specific item
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk) #passing pk=pk as we are getting a specific item
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk) #passing pk=pk as we are UPDATING a specific item
+        serializer = MovieSerializer(movie, data=request.data) #pass movie in serializer so it updates that specific pk
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    
+    elif request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk) #passing pk=pk as we are DELETING a specific item
+        movie.delete() #regular quearyset, no serialization involved
+        return Response(status=status.HTTP_204_NO_CONTENT)
